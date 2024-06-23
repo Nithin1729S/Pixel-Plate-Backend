91029f27a047cca0ace51002a173567f0d74d225
nithin <sureshnithin1729@gmail.com>
2024-06-23 14:08:17 +0530

    search bar

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import mongoose from "mongoose"
 import {v2 as cloudinary} from "cloudinary";
 import MyRestaurantController from "./controllers/MyRestaurantController"
 import MyRestaurantRoute from "./routes/MyRestaurantRoute"
+import restaurantRoute from './routes/RestaurantRoute'
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
     console.log("MONGODB Connected")
 })
@@ -24,6 +26,7 @@ app.get("/health",async(req:Request,res:Response)=>{
 });
 app.use("/api/my/user",myUserRoute);
 app.use("/api/my/restaurant",MyRestaurantRoute)
+app.use("/api/restaurant",restaurantRoute)
 
 app.listen(7000, () => {
     console.log("Server started on localhost 7000")
