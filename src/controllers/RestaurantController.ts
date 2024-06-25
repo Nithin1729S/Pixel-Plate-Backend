532135da1675466b0b0374e91b992e3b68927ed8
nithin <sureshnithin1729@gmail.com>
2024-06-25 17:37:39 +0530

    add details

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -13,7 +13,7 @@ const getRestaurant = async (req: Request, res: Response) => {
     res.json(restaurant);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "something went wrong" });
   }
 };
 
